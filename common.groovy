260efc2a72ada179a492a84f6dc47f9083cc8de0
sarawut_o_doppiotech <golfs.sarawut@gmail.com>
2024-10-25 17:51:39 +0700

    update

--- common.groovy
+++ common.groovy
@@ -1,7 +1,7 @@
 def updateGithubStatus(state) {
     withCredentials([usernamePassword(credentialsId: 'igollfs_cicd_pipeline', usernameVariable: 'USER', passwordVariable: 'PASSWORD')]) {
         sh """
-            curl --location 'https://api.github.com/repos/${USER}/jenkins_training/statuses/${GIT_COMMIT}' \
+            curl --location 'https://api.github.com/repos/igollfs/Learning_cicd_pipeline_process/statuses/${GIT_COMMIT}' \
             --header 'Content-Type: application/json' \
             --header 'Authorization: Bearer ${PASSWORD}' \
             --data '{"state": "${state}","context": "Dryrun","description": "Jenkins","target_url": "${BUILD_URL}"}'
